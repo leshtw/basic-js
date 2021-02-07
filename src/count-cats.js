98ae923dbb3b1e406c52de10148dfeddcea91d73
Alex Boh <cmextw2610@yandex.ru>
2021-02-08 01:10:59 +0300

    feat: add solution for hanoi tower

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(cat) {
+  let i = 0;
+  for (let item of cat) {
+    for (let el of item) {
+      if (el==='^^') {
+        i++;
+      }
+    }
+  }
+  return i;
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)) throw Error;
+  let result = [];
+  for(let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        if(i + 1 < arr.length) {
+          ++i;  // element will be passed
+          if(arr[i + 1] == '--double-prev' || arr[i + 1] == '--discard-prev')
+            ++i;                                                              
+          break;
+        }
+      case '--double-next':
+        if(i + 1 < arr.length)
+          result.push(arr[i + 1]); break;
+      case '--discard-prev':
+        if(result.length)
+          result.pop(); break;
+      case '--double-prev':
+        if(result.length)
+          result.push(result[result.length - 1]); break;
+      default:
+        result.push(arr[i]); break;
+    }
+  }
+  return result
 };
