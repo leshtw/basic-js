6d9faa46781d8acce5c561fe914bb28f8dd01507
Alex Boh <cmextw2610@yandex.ru>
2021-03-07 03:24:20 +0300

    feat: add solution for recursive-depth

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,10 +5,10 @@ const HALF_LIFE_PERIOD= 5730;
 
   module.exports = function dateSample(sampleActivity) {
     let N = parseFloat(sampleActivity),
-        k = (0.693 / HALF_LIFE_PERIOD);
+    k = (0.693 / HALF_LIFE_PERIOD)
 
-    if (typeof sampleActivity !== 'string' || Object.is(NaN, N) || N <= 0 || 15 <= N)
-      return false;
- 
-    return Math.ceil(Math.log(MODERN_ACTIVITY / N) / k);
+  if (typeof sampleActivity !== 'string' || Object.is(NaN, N) || N <= 0 || 15 <= N)
+    return false;
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / N) / k);
   };
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
   }
+
+  calculateDepth(arr) {
+    let Max=1;
+    arr.map((item)=>{
+      let currentDepth = 1;
+        if(Array.isArray(item)) {
+          currentDepth+= this.calculateDepth(item);
+            Max = Math.max(Max, currentDepth)
+        }
+    })
+    return Max
+}
 };
