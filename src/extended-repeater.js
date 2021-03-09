477d3245d0bc3c0eacdde916102a996292e950df
Alex Boh <cmextw2610@yandex.ru>
2021-03-10 01:43:08 +0300

    feat: add solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,31 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str = '', options) {
+  let pushStringArray = [];
+  let addition;
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (options.addition == false) {
+    addition = 'false';
+  } else if (options.addition === null) {
+    addition = 'null';
+  } else {
+    addition = options.addition || '';
+  }
+
+
+  let repeatTimes = options.repeatTimes || 1;
+  let separator = options.separator || '+';
+
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let additionSeparator = options.additionSeparator || '|';
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    pushStringArray.push(`${addition}`);
+  }
+
+  let addString = pushStringArray.join(additionSeparator);
+  let result = [];
+
+  for (let i = 0; i < repeatTimes; i++) {
+    result.push(`${str}${addString}`);
+  }
+  return result.join(separator);
 };
-  
