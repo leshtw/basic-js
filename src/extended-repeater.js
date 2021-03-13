c903a2170af0c2d9a6d44e7535df3c02edef0c30
Alex Boh <cmextw2610@yandex.ru>
2021-03-13 06:31:17 +0300

    feat: add solution vigenere-chipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,4 @@
-module.exports = function repeater(str = '', options) {
+module.exports = function repeater(str, options) {
   let pushStringArray = [];
   let addition;
 
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,77 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
+    this.alphabet = "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  encrypt(text, key) {
+    let resultIndex;
+    let textChipher = "";
+    let keyIndex = 0;
+    text = text.toLowerCase();
+    key = key.toLowerCase();
+    for (let i = 0; i < text.length; i++) {
+    let  textSim = text[i];
+      if (keyIndex > key.length - 1) {
+        keyIndex = 0;
+      }
+      if (!this.alphabet.includes(textSim)) {
+        textChipher += textSim;
+      }else{
+
+      let keyLetter = key[keyIndex];
+
+      let keyChiperIndex = this.alphabet.indexOf(keyLetter);
+      let letterChiperIndex = this.alphabet.indexOf(textSim);
+
+    let  resultIndex = keyChiperIndex + letterChiperIndex;
+      if (resultIndex >=26){
+        resultIndex= resultIndex-26;
+      }
+      let encryptedLetter = this.alphabet[resultIndex];
+
+      textChipher = textChipher + encryptedLetter ;
+
+      keyIndex+=1;
+    }
+    }
+
+    return (this.type === true) ?  textChipher.toUpperCase(): textChipher.toUpperCase().split('').reverse().join('') ;
+  }
+
+  decrypt(text, key) {
+    let resultIndex;
+    let textChipher = "";
+    let keyIndex = 0;
+    text = text.toLowerCase();
+    key = key.toLowerCase();
+    for (let i = 0; i < text.length; i++) {
+    let  textSim = text[i];
+      if (keyIndex > key.length - 1) {
+        keyIndex = 0;
+      }
+      if (!this.alphabet.includes(textSim)) {
+        textChipher += textSim;
+      }else{
+
+      let keyLetter = key[keyIndex];
+
+      let keyChiperIndex = this.alphabet.indexOf(keyLetter);
+      let letterChiperIndex = this.alphabet.indexOf(textSim);
+
+    let  resultIndex = letterChiperIndex - keyChiperIndex ;
+      if (resultIndex <0){
+        resultIndex= resultIndex+26;
+      }
+      let encryptedLetter = this.alphabet[resultIndex];
+
+      textChipher = textChipher + encryptedLetter ;
+
+      keyIndex+=1;
+    }
+    }
+
+    return (this.type === true) ?  textChipher.toUpperCase(): textChipher.toUpperCase().split('').reverse().join('') ;
   }
 }
 
